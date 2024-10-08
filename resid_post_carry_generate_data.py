348fea9039f4f0a8cdcf5ce4f9617fc3b0d8fe8f
Aylin Akkus <aylin.g.akkus@gmail.com>
2024-10-08 16:08:34 +0200

    Added files to predict carry digit feature from resid_mid and resid_post

---
+++ resid_post_carry_generate_data.py
@@ -0,0 +1,93 @@
+import torch as t
+from config import Config
+from model import Transformer 
+from tokenizer import Tokenizer
+import pandas as pd
+
+def carry_digits(row, max_len):
+    """
+    Checks whether there is a carry digit at position n in the result of the addition
+    """
+    # Read the operands
+    operand_1 = str(row["operand_1"])
+    operand_2 = str(row["operand_2"])
+
+    # Pad the operands with 0s
+    paddded_op_1 = operand_1.zfill(max_len)
+    padded_op_2 = operand_2.zfill(max_len)
+    # Check for all carry digits
+    carries = [0] * (max_len-1)
+    for i in range(max_len):
+        if int(paddded_op_1[-(i+1)]) + int(padded_op_2[-(i+1)]) >= 10:
+            carries[-(i+1)] = 1
+
+    assert len(carries) == max_len-1
+    return carries
+   
+def add_carry_columns_to_df(data_path, new_data_path=None):
+    df = pd.read_csv(data_path)
+    max_len = df["result"].apply(lambda x: len(str(x))).max()
+    carry_col_names = [f"carry_{i}" for i in range(1, max_len)]
+    res = df.apply(lambda x: carry_digits(x, max_len), axis=1)
+    df[carry_col_names] = pd.DataFrame(res.tolist(), index=df.index)
+    if new_data_path:
+        df.to_csv(new_data_path, index=False)
+    else:
+        df.to_csv(data_path, index=False)
+
+# Load the model
+config = Config()
+model_path = "saved_runs/variable_digit_add_50/final.pth"
+data_path = "saved_runs/variable_digit_add_50/data.csv"
+device = config.device
+tokenizer = Tokenizer(Config())
+model = Transformer(Config())
+model.load_state_dict(t.load(model_path, map_location = device)["model"])
+model.eval()
+
+hidden_dict = {}
+
+def get_hidden_equals(idx, activation, **kwargs):
+    name = kwargs.get('name', 'Unknown')
+    activation = activation.squeeze() # Remove the batch dimension
+    hidden_equals = activation[-1]
+    hidden_dict[idx] = hidden_equals
+
+def get_hidden_equals_with_idx(idx):
+    return lambda activation, **kwargs: get_hidden_equals(idx, activation, **kwargs)
+
+data = pd.read_csv(data_path)
+for idx, row in data.iterrows():
+    # Adding the hook to the model
+    print(f"idx: {idx}")
+    model.blocks[-1].hook_resid_post.add_hook(get_hidden_equals_with_idx(idx))
+
+    # Tokenizing the sentence
+    op_1 = row["operand_1"]
+    op_2 = row["operand_2"]
+    input_sentence = f"{op_1}+{op_2}="
+    input_sentence_tokenized = [tokenizer.tokenize(input_sentence)]
+    input_sentence_tokenized = t.tensor(input_sentence_tokenized).to(device)
+
+    # Forward pass
+    with t.no_grad():
+        output = model(input_sentence_tokenized)
+
+    model.remove_all_hooks()
+
+    # Merge the dict into the dataframe
+d_model = config.d_model
+col_list = [f"hidden_{i}" for i in range(d_model)]
+
+for idx, hidden_state in hidden_dict.items():
+    hidden_state = hidden_state.cpu().tolist()
+    data.loc[idx, col_list] = hidden_state
+
+# Write the dataframe to a csv file
+
+# Drop the columns that are not needed
+new_data_path = "saved_runs/variable_digit_add_50/resid_post_carry_log_reg_data.csv"
+data_2 = data.drop(["Unnamed: 0", "input_str", "tokenized"], axis=1)
+data_2.to_csv(new_data_path, index=False)
+# Add the carry columns
+add_carry_columns_to_df(new_data_path)
---
+++ resid_post_log_reg_carry_digit.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import matplotlib
+print(matplotlib.get_backend())
+import pandas as pd
+from config import Config
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv('saved_runs/variable_digit_add_50/resid_post_carry_log_reg_data.csv')
+
+config = Config()
+d_model = config.d_model
+train_col_namews = [f"hidden_{i}" for i in range(d_model)]
+
+# Split data into train and test
+x_train = data[data["is_train"]][train_col_namews]
+x_test = data[~data["is_train"]][train_col_namews]
+y_train = data[data["is_train"]]["carry_1"]
+y_test = data[~data["is_train"]]["carry_1"]
+
+def get_accuracy(log_reg, x_test, y_test):
+    y_pred = log_reg.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
+
+def get_number_of_non_zero_coefficients(log_reg):
+    log_reg.coef_ = log_reg.coef_.reshape(-1)
+    non_zero_indices = [i for i, x in enumerate(log_reg.coef_) if x != 0]
+    return len(non_zero_indices)
+
+def get_top_n_coef(log_reg, n):
+    log_reg.coef_ = log_reg.coef_.reshape(-1)
+    coef = log_reg.coef_
+    # Sort the coefficients in descending order and get the top n
+    top_n_indices = np.argsort(coef)[::-1][:n]
+    return top_n_indices
+
+# Plot the accuracy of the model for different values of C
+c_values = np.linspace(0.0001, 0.01, 100)
+accuracies = []
+num_non_zero = []
+
+for c in c_values:
+    log_reg = LogisticRegression(penalty='l1', solver='liblinear', C=c)
+    log_reg.fit(x_train, y_train)
+    # Calculate the accuracy
+    accuracy = get_accuracy(log_reg, x_test, y_test)
+    accuracies.append(accuracy*100)
+
+    # Calculate the number of non-zero coefficients
+    num_coef = get_number_of_non_zero_coefficients(log_reg)
+    num_non_zero.append(num_coef)
+
+top_3 = get_top_n_coef(log_reg, 3)
+print(f"Top 3 coefficients: {top_3}")
+
+# Simple plot to test
+fig, ax = plt.subplots()
+ax.plot(c_values, accuracies, label='Accuracy', color='b')
+ax.set_xlabel('L1 penalty values')
+ax.set_ylabel('Accuracy', color='b')
+ax2 = ax.twinx()
+ax2.plot(c_values, num_non_zero, label='Number of non-zero coefficients', color='r')
+ax2.set_ylabel('Number of non-zero coefficients', color='r')
+ax.legend()
+plt.show()
